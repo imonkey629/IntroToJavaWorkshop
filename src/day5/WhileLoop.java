1793e85983465fe5c004e530a47c1c8490034658
imonkey629 <league@FunkyMini.local>
2016-10-08 11:33:02 -0700

    10-08-16
    while loop

---
+++ src/day5/WhileLoop.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class WhileLoop {
+public static void main(String[] args) {
+	int thing=-9;
+	while(thing<=-5){
+	thing=thing+1;
+	
+	System.out.print(thing+",");
+	}
+	
+}
+}
---
+++ src/day5/joke.java
@@ -0,0 +1,14 @@
+package day5;
+
+import javax.swing.JOptionPane;
+
+public class joke {
+public static void main(String[] args) {
+	int numTimes=1;
+	while(numTimes<=4){
+numTimes=numTimes+1;
+	JOptionPane.showMessageDialog(null,"what is a box full of gold that can't be opened with a key?");
+JOptionPane.showMessageDialog(null, "an egg");
+	}
+}	
+}
