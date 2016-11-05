e4ffc3058f1117437358c779821aa1dd1b451e59
imonkey629 <league@Mac-mini-3.local>
2016-11-05 11:37:53 -0700

    njkfdhguirdhguiojbgoi

---
+++ src/YearsAlive.java
@@ -0,0 +1,14 @@
+
+public class YearsAlive {
+public static void main(String[] args) {
+	int num = 0;
+	while(num<=14){
+		System.out.println(num);
+		num = num +1;
+	}
+	for(int diego=0; diego<=14;diego=diego+1){
+		System.out.println(diego);
+	}
+}
+}
+  
---
+++ src/forLoopChalllege.java
@@ -0,0 +1,11 @@
+
+public class forLoopChalllege {
+public static void main(String[] args) {
+	for(int david=2;david<=10; david =david+2){
+	System.out.println(david);
+	}
+	for(int john= 99; john>=94; john=john-1){
+		System.out.println(john);
+	}
+}
+}
